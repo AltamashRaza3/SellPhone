7cb75e77aa24db1f79d9375cde3678cb75a2b00a
Altamash Raza <altamashraza3.ar@gmail.com>
2026-02-06 02:11:03 +0530

    ADMINlOGIN

--- client/sellphone/src/pages/Admin/AdminLogin.jsx
+++ client/sellphone/src/pages/Admin/AdminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import API_BASE_URL from "../config/api";
+import API_BASE_URL from "../../config/api";
 
 const AdminLogin = () => {
   const navigate = useNavigate();
